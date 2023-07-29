4a2643318d4148ba60122ba1aa78e3fd37e0896f
andresreyesk2D <andres.reyes@k2digital.io>
2023-07-29 11:15:31 -0500

    Feat: Creación de proyecto y componentes, se realizan los ajustes necesarios para trabajar el proyecto con una de las versiónes de angular propuestas en este caso angular v12.0.0 y node v12.22.12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { XyzComponent } from './xyz/xyz.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { SearchFormComponent } from './components/partials/search-form/search-form.component';
+import { FormSearchComponent } from './components/partials/form-search/form-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XyzComponent,
+    HeaderComponent,
+    SearchFormComponent,
+    FormSearchComponent
   ],
   imports: [
     BrowserModule,
