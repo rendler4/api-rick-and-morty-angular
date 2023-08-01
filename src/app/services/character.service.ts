54dc8bdd7d6135bb15250dcad9d55d0270d38b46
andresreyesk2D <andres.reyes@k2digital.io>
2023-07-31 23:53:13 -0500

    Feat: Refinamientos visuales btn back

--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -14,7 +14,6 @@ export class CharacterService {
   searchCharacters(query = '', page=1 ){
     //return this.http.get<Character[]>(`${environment.baseUrlAPI}/?name=${query}&page=${page}`);
     const selectFilterCampo = ((<HTMLInputElement>document.getElementById("selectCampofilter"))?.value);
-    alert(selectFilterCampo)
     return this.http.get<Character[]>(`https://rickandmortyapi.com/api/character/?${selectFilterCampo===undefined?'name':selectFilterCampo}=${query}&page=${page}`);
   }
 
